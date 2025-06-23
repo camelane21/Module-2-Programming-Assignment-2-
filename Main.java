48df4ca829ec3ca0d8cb0e37bdbace2d143d642e
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-23 14:08:40 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -12,3 +12,23 @@ abstract class GeometricObject {
         this.color = color;
         this.filled = filled;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
