526fd0f61b5e47b5db48ffae791bc244122b3080
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-23 21:22:33 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -58,3 +58,20 @@ class Triangle extends GeometricObject {
     public double getSide3() {
         return side3;
     }
+
+ @Override
+    public double getArea() {
+        double s = getPerimeter() / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+    }
+}
