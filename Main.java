aec9029fa92847d2a2f483b8db89a3ff9b4053cc
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-23 21:20:30 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -32,3 +32,17 @@ abstract class GeometricObject {
     public abstract double getArea();
     public abstract double getPerimeter();
 }
+
+// Triangle subclass
+class Triangle extends GeometricObject {
+    private double side1 = 1.0;
+    private double side2 = 1.0;
+    private double side3 = 1.0;
+
+    public Triangle() {}
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
