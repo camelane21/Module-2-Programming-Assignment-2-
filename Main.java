6fe7b4e332ab5f6067ba55c2b275bd59f1f2925d
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-23 21:21:26 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -46,3 +46,15 @@ class Triangle extends GeometricObject {
         this.side2 = side2;
         this.side3 = side3;
     }
+
+ public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
