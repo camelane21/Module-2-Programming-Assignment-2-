af8885d269541cfe8da9144c03ac3ce9694f75b8
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-23 21:24:06 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -75,3 +75,35 @@ class Triangle extends GeometricObject {
         return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
     }
 }
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter side1: ");
+        double side1 = input.nextDouble();
+
+        System.out.print("Enter side2: ");
+        double side2 = input.nextDouble();
+
+        System.out.print("Enter side3: ");
+        double side3 = input.nextDouble();
+
+        System.out.print("Enter the color: ");
+        String color = input.next();
+
+        System.out.print("Is the triangle filled? (true/false): ");
+        boolean filled = input.nextBoolean();
+
+        Triangle triangle = new Triangle(side1, side2, side3);
+        triangle.setColor(color);
+        triangle.setFilled(filled);
+
+        System.out.println("\n" + triangle.toString());
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+        System.out.println("Color: " + triangle.getColor());
+        System.out.println("Filled: " + triangle.isFilled());
+    }
+}
